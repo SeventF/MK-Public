799d1380a1f148cef6e4ee4e2a8e0db5a69a08d4
SeventF <nick87nrg@gmail.com>
2019-06-20 23:39:44 +0700

    the project of clock on atmel atmega16a-PU

---
+++ Clock on atmega16a-PU/CLOCK16A/LCD1602.h
@@ -0,0 +1,80 @@
+﻿#include <stdlib.h>
+#include <util\delay.h>
+
+#define RS   PORTD0
+#define E	 PORTD1
+#define DB4  PORTD4
+#define DB5  PORTD5
+#define DB6  PORTD6
+#define DB7  PORTD7
+
+void LCD_INI();
+void SendHalfByte(unsigned char c);
+void SendByte(unsigned char c, unsigned char mode);
+void SendChar(unsigned char c);
+void SetPos(unsigned char x,unsigned char y);
+void SendStr(char str[]);
+void SetDdramAddress(uint8_t address);
+void SetCgramAddress(uint8_t address);
+
+//--------------------------------
+void LCD_INI(){
+	_delay_ms(40);
+	SendHalfByte(0b00000011);
+	_delay_ms(5);
+	SendHalfByte(0b00000011);
+	_delay_us(100);
+	SendHalfByte(0b00000011);
+	_delay_ms(1);
+	SendHalfByte(0b00000010);
+	_delay_ms(1);
+	SendByte(0b00101000, 0); // to switch on 4-bit (DL=0) the mode and in 2 lines (N=1)
+	_delay_ms(1);
+	SendByte(0b00001100, 0); // to switch on the image on the display (D=1), cursors are switched-off (C=0,B=0)
+	_delay_ms(1);
+	SendByte(0b0000110, 0); // the cursor (invisible) moves to the left
+	_delay_ms(1);
+}
+//--------------------------------
+void SendHalfByte(unsigned char c){
+	c<<=4;
+	PORTD |=(1<<E);	 //to switch on line E
+	_delay_us(50);
+	PORTD &=~((1<<DB4)|(1<<DB5)|(1<<DB6)|(1<<DB7));
+	PORTD |= c;
+	PORTD &=~(1<<E); //to switch off line E
+	_delay_us(50);
+}
+//--------------------------------
+void SendByte(unsigned char c, unsigned char mode){
+	unsigned char hc;
+	if(mode==0) PORTD &=~(1<<RS); //RS == 0 (command)
+	else PORTD |=(1<<RS);		  //RS == 1 (data)
+	hc = c>>4;
+	SendHalfByte(hc); //send younger half-byte
+	SendHalfByte(c); //send the senior half-byte
+}
+//--------------------------------
+void SendChar(unsigned char c){
+	SendByte(c, 1); // byte with data
+}
+//--------------------------------
+void SetPos(unsigned char x,unsigned char y){
+	char adress;
+	adress=(0x40*y+x)|0b10000000;
+	SendByte(adress, 0);
+}
+//---------------------------------
+void SendStr(char str[]){
+	wchar_t n;
+	for(n=0;str[n]!='\0';n++)
+	SendChar(str[n]);
+}
+//--------------------------------
+void SetDdramAddress(uint8_t address){
+	SendByte(address|0x80,0);
+}
+//--------------------------------
+void SetCgramAddress(uint8_t address){
+	SendByte(address|0x40,0);
+}
---
+++ Clock on atmega16a-PU/CLOCK16A/main.c
@@ -0,0 +1,110 @@
+/*
+ * simple clock on atmega 16a
+ *
+ * Created: 20.06.2019 17:28:57
+ * Author : SeveNT
+ */ 
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "LCD1602.h"
+
+uint8_t ClockSym[8]= {0x0,0xE,0x15,0x17,0x11,0xE,0x0};// character of clock
+//uint8_t  Dynamic[8]= {0x1,0x12,0x18,0x1b,0x18,0x12,0x1};//alarm clock character
+unsigned char Sec,Min,Hour = 0;
+char TimeBuffer[2];
+
+void TIMER_INI();
+void PORTS_INI();
+
+int main(void)
+{
+	DDRC = 0xFF;
+	
+	PORTS_INI();
+	LCD_INI();
+	TIMER_INI();
+	SetPos(1, 0);
+	SendStr("00:00:00");
+	SetCgramAddress(0x0F);
+	for(uint8_t i=0;i<8;i++){
+		SendByte(ClockSym[i],1);
+	}	
+	SetDdramAddress(0x08);
+	SetPos(0,0);
+	SendByte(0x02,1);
+	/*---------------alarm clock character output block------------
+	SetCgramAddress(0x08);
+	for(uint8_t i=0;i<8;i++){
+		SendByte(Dynamic[i],1);
+	}	
+	SetDdramAddress(0x00);
+	SetPos(9,0);
+	SendByte(0x01,1);		
+	*///-------------alarm clock character output block----------
+		
+	while (ASSR&((1<<TCN2UB)|(1<<OCR2UB)|(1<<TCR2UB))); //We wait for readiness of Timer2
+	TIMSK |=(1<<TOIE2);// Include interruption on overflow
+	sei(); //Allow interruptions
+	
+    /* Replace with your application code */
+ while (1)
+ {
+	 if(!(PINA&(1<<PORTA1))){
+		 _delay_ms(400); //Remove noise of the button
+		 Hour++;
+		 Sec=0;
+		 if(Hour<=9) {SetPos(2,0);SendStr(itoa(Hour,TimeBuffer,10));}
+		  else       {SetPos(1,0);SendStr(itoa(Hour,TimeBuffer,10));}
+	 }
+	 if(!(PINA&(1<<PORTA2))){
+		 _delay_ms(400); //Remove noise of the button-----------------+
+		 Min++;
+		 Sec=0;
+		 if(Min<=9)  {SetPos(5,0);SendStr(itoa(Min,TimeBuffer,10));}
+		  else       {SetPos(4,0);SendStr(itoa(Min,TimeBuffer,10));}
+	 }
+
+ }
+ }
+//----------------------------
+void TIMER_INI(){
+	TIMSK &=~(1<<OCIE2)|(1<<TOIE2);//Prohibit interruptions of Timer2
+	ASSR |=(1<<AS2);// Include an asynchronous mode of external quartz
+	TCNT2 = 0x00; //We reset the counter
+	TCCR2 |=(1<<CS20)|(1<<CS22);//Installation of a predivider (32768/128=256tiks per second)
+}
+//----------------------------
+ISR(TIMER2_OVF_vect){
+	Sec++;
+	
+	if(Sec<=9){SetPos(8,0);SendStr(itoa(Sec,TimeBuffer,10));}
+	 else	  {SetPos(7,0);SendStr(itoa(Sec,TimeBuffer,10));}
+	if(Sec==60){
+		Sec=0; SetPos(7,0);SendStr(itoa(Sec,TimeBuffer,10));
+		Min++;
+		if(Min<=9){SetPos(5,0);SendStr(itoa(Min,TimeBuffer,10));}
+		 else     {SetPos(4,0);SendStr(itoa(Min,TimeBuffer,10));}
+	}
+	if(Min==60){
+		Min=0; SetPos(4,0);SendStr(itoa(Min,TimeBuffer,10));
+		Hour++;
+		if(Hour<=9){SetPos(2,0);SendStr(itoa(Hour,TimeBuffer,10));}
+		 else      {SetPos(1,0);SendStr(itoa(Hour,TimeBuffer,10));}
+	}
+	if(Hour==24) {Sec=0;Min=0;Hour=0;SetPos(1,0);SendStr("00:00:00");}
+	if(CheckDots){SetPos(3,0);SendStr(" ");SetPos(6,0);SendStr(" ");CheckDots=false;}
+	 else        {SetPos(3,0);SendStr(":");SetPos(6,0);SendStr(":");CheckDots=true;}
+}
+//----------------------------
+void PORTS_INI(){
+		DDRD  = 0xFF;
+		PORTD = 0x00;
+		DDRA  = 0x00;
+		PORTA|=(1<<PORTA1)|(1<<PORTA2);			
+}
+//----------------------------
